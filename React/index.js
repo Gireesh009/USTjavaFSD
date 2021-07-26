5a9d81e2c05bc01908e3208a43a5ca1012f6018b
Gireesh009 <68183959+Gireesh009@users.noreply.github.com>
2021-07-26 15:24:07 +0530

    Add files via upload
    React programs

---
+++ React/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import FlavorForm from './Submit';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FlavorForm />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
